d60bd2cde493769f8ccd158192504cbb49c1df86
JhonyRa <43555988+JhonyRa@users.noreply.github.com>
2020-06-11 14:19:24 -0300

    @Data funcionando
    Lombok funcionou com sucesso, até me emocionei

--- src/main/java/com/jhonatanaguiar/clothingstore/business/bean/ClothesBean.java
+++ src/main/java/com/jhonatanaguiar/clothingstore/business/bean/ClothesBean.java
@@ -1,5 +1,16 @@
 package com.jhonatanaguiar.clothingstore.business.bean;
 
+import lombok.Data;
+
+@Data
 public class ClothesBean {
+	
+	private Integer code;
+	private String name;
+	private Integer codeCategory;
+	private String category;
+	private Float price;
+	private String nameImage;
 
 }
+	
